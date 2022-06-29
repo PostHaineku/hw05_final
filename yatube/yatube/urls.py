d0989d345f50f02aef62fbaafff71274b765981b
Влад Бочкарёв <ichi222@yandex.ru>
2022-06-29 21:46:11 +0300

    try

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -11,7 +11,19 @@ urlpatterns = [
     path("about/", include("about.urls", namespace="about")),
 ]
 
-handler404 = 'core.views.page_not_found'
+handler404 = "core.views.page_not_found"
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+handler500 = "core.views.page_500"
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+handler403 = "core.views.page_403"
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
