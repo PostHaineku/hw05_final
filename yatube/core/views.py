d33a5d9287eaa0897eb6a1845d81e14d7b9bf49d
Влад Бочкарёв <ichi222@yandex.ru>
2023-03-03 13:57:58 +0300

    core_views_changed

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
 
-def page_not_found(request, exception):
+def page_not_found(request, *args, **argv):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def csrf_failure(request, reason=''):
+def csrf_failure(request, *args, **argv):
     return render(request, 'core/403csrf.html')
 
 
-def page_500(request, exception):
+def page_500(request, *args, **argv):
     return render(request, 'core/500.html', {'path': request.path}, status=500)
 
 
-def page_403(request, exception):
+def page_403(request, *args, **argv):
     return render(request, 'core/403.html', {'path': request.path}, status=403)
